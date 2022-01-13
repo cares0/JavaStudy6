d40f8f0ecb7cb2364cf726d5be85fc38e157623e
cares0 <cares00000@gmail.com>
2022-01-13 16:34:09 +0900

    Add DTO

---
+++ src/com/cares/ex1/data/PersonDTO.java
@@ -0,0 +1,43 @@
+package com.cares.ex1.data;
+
+public class PersonDTO {
+
+	private String name;
+	private String nickname;
+	private String email;
+	private String phoneNum;
+	private String birthday;
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhoneNum() {
+		return phoneNum;
+	}
+	public void setPhoneNum(String phoneNum) {
+		this.phoneNum = phoneNum;
+	}
+	public String getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+	
+	
+}
