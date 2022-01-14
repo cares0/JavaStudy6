8e164c29c24dbee087123279dfa02f15f0257132
cares0 <cares00000@gmail.com>
2022-01-14 17:50:19 +0900

    __

--- src/com/cares/ex1/main/AdressBookMain.java
+++ src/com/cares/ex1/main/AdressBookMain.java
@@ -1,6 +1,7 @@
 package com.cares.ex1.main;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import com.cares.ex1.data.DataMethod;
 import com.cares.ex1.data.PersonDTO;
@@ -9,9 +10,16 @@ import com.cares.ex1.func.Function;
 public class AdressBookMain {
 
 	public static void main(String[] args) {
-		Function function = new Function();
-		
+		Function function = new Function();		
 		function.menuSelect();
+		
+		
+		ArrayList<Integer> ar = new ArrayList<Integer>();
+		ArrayList<Integer> ar2 = new ArrayList<Integer>();
+		
+		HashMap<String, ArrayList<Integer>> map = new HashMap<>();
+		map.put("a", ar);
+		map.put("b", ar2);
 		
 		
 		
